76b982ad72cf9dc361bd15d669fba9d77d2aa8d0
Ketan Agnihotri <agnihotriketan@users.noreply.github.com>
2022-09-08 19:15:41 +0530

    CountSubIslands

--- Study/Program.cs
+++ Study/Program.cs
@@ -1916,5 +1916,54 @@ namespace Study
 
             return fresh == 0 ? time - 1 : -1;
         }
+
+        public bool CarPooling(int[][] trips, int capacity)
+        {
+            var dictionary = new SortedDictionary<int, int>();
+
+            foreach (var trip in trips)
+            {
+                if (!dictionary.ContainsKey(trip[1])) dictionary[trip[1]] = 0;
+                if (!dictionary.ContainsKey(trip[2])) dictionary[trip[2]] = 0;
+                dictionary[trip[1]] += trip[0]; dictionary[trip[2]] -= trip[0];
+            }
+            int currentPassennger = 0;
+            foreach (var keyval in dictionary)
+            {
+                currentPassennger += keyval.Value;
+                if (currentPassennger > capacity) return false;
+            }
+            return true;
+        }
+
+        public int CountSubIslands(int[][] grid1, int[][] grid2)
+        {
+            int r = grid1.Length, c = grid1[0].Length, subIslands = 0;
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < grid2[r].Length; j++)
+                {
+                    if (grid2[i][j] == 1 && DfsSubIsLand(grid1, grid2, r, c))
+                    {
+                        subIslands++;
+                    }
+                }
+            }
+            return subIslands;
+        }
+
+        private bool DfsSubIsLand(int[][] grid1, int[][] grid2, int r, int c)
+        {
+            if (r < 0 || c < 0 || r >= grid2.Length || c >= grid2[r].Length || grid2[r][c] == 0) { return true; }
+
+            grid2[r][c] = 0;
+            bool stillASubIsland = true;
+            stillASubIsland &= DfsSubIsLand(grid1, grid2, r-1, c);
+            stillASubIsland &= DfsSubIsLand(grid1, grid2, r, c+1);
+            stillASubIsland &= DfsSubIsLand(grid1, grid2, r+1, c);
+            stillASubIsland &= DfsSubIsLand(grid1, grid2, r, c-1); 
+
+            return stillASubIsland && grid1[r][c] == 1;
+        }
     }
 }
